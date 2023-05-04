8fb12438b76936c3f9b89406acea16c5b950f458
Pranati <pranat.nayak2005@gmail.com>
2023-05-04 17:21:43 +0530

    second commit

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.API.Models;
+using Shopping.API.Data;
+using MongoDB.Driver;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ProductContext _productContext;
+        private readonly ILogger<ProductController> logger;
+
+        public ProductController(ProductContext productContext, ILogger<ProductController> logger)
+        {
+            _productContext = productContext ?? throw new ArgumentNullException(nameof(productContext));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetProducts() {
+           
+            return await _productContext.Products.Find(p => true).ToListAsync();
+        }
+    }
+}
--- Shopping/Shopping.Client/Program.cs
+++ Shopping/Shopping.Client/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddHttpClient("ShoppingAPIClient", client =>
+{
+    //client.BaseAddress = new Uri("http://localhost:5000/");
+    client.BaseAddress = new Uri(builder.Configuration["ShoppingAPIUrl"]);
+    // Add services to the container.
+});
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
